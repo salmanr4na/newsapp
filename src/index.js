1db02a5818c6ff4808f3c0fded35b305585d8646
Salman Rana <raosalmantnb@gmail.com>
2025-04-08 13:01:24 +0530

    final changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Components/App'
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap/dist/js/bootstrap.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
